9d9f287d774656e0813267438083419479e14b24
Michelle Nguyen <mnguye96@ucsc.edu>
2016-01-30 20:58:42 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/michellenguyen/ome/MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.michellenguyen.ome;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    public static ArrayList<String> values = new ArrayList<String>(0);
+    //ArrayList<Payment> values;
+
+    //First We Declare Titles And Icons For Our Navigation Drawer List View
+    //This Icons And Titles Are holded in an Array as you can see
+
+    String TITLES[] = {"Profile", "Groups", "Search", "OMes"};
+    int ICONS[] = {R.drawable.ic_profiles,R.drawable.ic_groups,R.drawable.ic_search,R.drawable.ic_moneyc};
+    /* String TITLES[] = {"Home","Events","Mail","Shop","Travel"};
+    int ICONS[] = {R.drawable.ic_home,R.drawable.ic_events,R.drawable.ic_mail,R.drawable.ic_shop,R.drawable.ic_travel};
+    */
+
+    String NAME = "Dashboard";
+    private Toolbar toolbar;
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+    ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar =(Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,this);
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+        };
+        // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // set the drawer toggle sync State
+
+        /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+        */
+        Bundle userData = getIntent().getExtras();
+        if(userData != null){
+            String key = "name";
+            System.out.println(userData.getString(key));
+            values.add(userData.getString(key));
+        }
+
+
+Button oMe = (Button) findViewById(R.id.button2);
+        Button oYou = (Button) findViewById(R.id.button3);
+        listView = (ListView)findViewById(R.id.list);
+
+        //Defined Array values to show ListView
+        //setValue();
+        //values = new String[]{"txt1", "txt2", "txt3", "txt4"};
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            //ListView Clicked item index
+            int itemPosition = position;
+
+            //ListView Clicked item value
+            String itemValue = (String)listView.getItemAtPosition(position);
+
+            //Show Alert
+            Toast.makeText(getApplicationContext(),"Position :" + itemPosition + " ListItem : "
+                    + itemValue, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        oMe.setOnClickListener(new View.OnClickListener() {
+
+            /** Called when the user clicks the Make O-Me button **/
+            public void onClick(View view) {
+                //Do something in response to the button
+                ((ArrayAdapter) listView.getAdapter()).notifyDataSetChanged();
+                Intent intent = new Intent(getApplicationContext(), OMeMaker.class);
+                startActivity(intent);
+            }
+        });
+
+        oYou.setOnClickListener(new View.OnClickListener() {
+
+            /** Called when user clicks Make O-You button **/
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OYouMaker.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /*
+    public void setValue(){
+        values = new String[5];
+        for(int i = 0; i < values.length; i++){
+            values[i] = "value" + i;
+        }
+    }
+    */
+
+    public void addPayment(Payment newPayment){
+
+    }
+/*
+    public void submitOMe(View view){
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+    }
+*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
+
